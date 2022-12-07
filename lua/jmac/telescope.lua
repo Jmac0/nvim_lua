a24aa2b8ff047f8911f6c769d3e4d3139753ea7a
JamieMac <Jamiejmackenzie@gmail.com>
2022-12-07 13:21:55 +0000

    Added leader gs to grep string

--- lua/jmac/telescope.lua
+++ lua/jmac/telescope.lua
@@ -8,5 +8,6 @@ require('telescope').setup{defaults = {file_ignore_patterns = {"node_modules", "
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
