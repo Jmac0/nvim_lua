ac8a933c1fe6560d23527f1f20fb4ce96805796a
JamieMac <Jamiejmackenzie@gmail.com>
2023-01-03 19:52:52 +0000

    Remove emmet section as causing error

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,26 +2,6 @@
 require("luasnip.loaders.from_vscode").lazy_load()
 local luasnip = require("luasnip")
 luasnip.filetype_extend("typescript", { "typescript" })
--------------------------- Emmet ------------------------------------------- 
-local lspconfig = require('lspconfig')
-local configs = require('lspconfig/configs')
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-lspconfig.emmet_ls.setup({
-    -- on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
-    init_options = {
-      html = {
-        options = {
-          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-          ["bem.enabled"] = true,
-        },
-      },
-    }
-})
--------------------------------------------------------------------------------
 
 
   cmp.setup({
